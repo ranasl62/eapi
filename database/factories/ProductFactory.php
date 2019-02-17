83835255929787317f34d148e5a9108c495b5f67
Md. Rana Hossain <ranasl62@gmail.com>
2019-02-18 01:52:54 +0600

    Created Model -a and api

---
+++ database/factories/ProductFactory.php
@@ -0,0 +1,9 @@
+<?php
+
+use Faker\Generator as Faker;
+
+$factory->define(App\Modal\Product::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
---
+++ database/factories/ReviewFactory.php
@@ -0,0 +1,9 @@
+<?php
+
+use Faker\Generator as Faker;
+
+$factory->define(App\Modal\Review::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
